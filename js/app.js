4b2526228ec4734a374a83f3e566ee1cd7defa60
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-01-22 15:41:38 +0200

    add my files

---
+++ js/app.js
@@ -0,0 +1,89 @@
+// my data
+const data = [
+  {
+    id:0,
+    title: "section1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, totam quis deserunt, velit nesciunt doloribus adipisci deleniti architecto sapiente porro nostrum voluptatum voluptates iste maiores iure voluptate. Recusandae quo iure, obcaecati omnis quaerat enim labore, cumque rerum commodi doloremque ad ipsum iusto possimus nam mollitia perferendis maiores error saepe debitis eaque repellat. Officia cum totam minus aliquam eos illum sapiente voluptatem vel inventore ad exercitationem distinctio ipsum rerum incidunt, cupiditate laudantium, tempore quaerat? Ducimus, error at, exercitationem, incidunt nihil suscipit maxime vel voluptas aperiam voluptatem sit iure esse voluptatum. Ipsa dolor nihil corporis sed nemo beatae, necessitatibus delectus asperiores molestias recusandae ea modi ad pariatur fugit eveniet quisquam consectetur vel harum laudantium itaque qui dolores consequatur magnam alias. Recusandae debitis rerum unde autem eaque assumenda vitae nostrum quia beatae ex repellat natus, porro voluptates ipsam facilis. Reprehenderit magnam explicabo voluptatum rem itaque totam hic. Ab, optio molestiae reprehenderit distinctio ipsam repudiandae suscipit accusantium incidunt consectetur nostrum sit atque nisi voluptatum, corporis dolorem ea consequuntur aspernatur nulla facilis. Nostrum nihil fugit ipsa doloremque labore tenetur nemo consequuntur blanditiis ex odio, laborum ducimus distinctio voluptatem placeat perferendis! Aliquid nesciunt placeat eum nulla beatae distinctio cupiditate fugit blanditiis? Fugiat, asperiores saepe. Amet, aut.",
+  },
+  {
+    id:1,
+    title: "section2",
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, totam quis deserunt, velit nesciunt doloribus adipisci deleniti architecto sapiente porro nostrum voluptatum voluptates iste maiores iure voluptate. Recusandae quo iure, obcaecati omnis quaerat enim labore, cumque rerum commodi doloremque ad ipsum iusto possimus nam mollitia perferendis maiores error saepe debitis eaque repellat. Officia cum totam minus aliquam eos illum sapiente voluptatem vel inventore ad exercitationem distinctio ipsum rerum incidunt, cupiditate laudantium, tempore quaerat? Ducimus, error at, exercitationem, incidunt nihil suscipit maxime vel voluptas aperiam voluptatem sit iure esse voluptatum. Ipsa dolor nihil corporis sed nemo beatae, necessitatibus delectus asperiores molestias recusandae ea modi ad pariatur fugit eveniet quisquam consectetur vel harum laudantium itaque qui dolores consequatur magnam alias. Recusandae debitis rerum unde autem eaque assumenda vitae nostrum quia beatae ex repellat natus, porro voluptates ipsam facilis. Reprehenderit magnam explicabo voluptatum rem itaque totam hic. Ab, optio molestiae reprehenderit distinctio ipsam repudiandae suscipit accusantium incidunt consectetur nostrum sit atque nisi voluptatum, corporis dolorem ea consequuntur aspernatur nulla facilis. Nostrum nihil fugit ipsa doloremque labore tenetur nemo consequuntur blanditiis ex odio, laborum ducimus distinctio voluptatem placeat perferendis! Aliquid nesciunt placeat eum nulla beatae distinctio cupiditate fugit blanditiis? Fugiat, asperiores saepe. Amet, aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex",
+  },
+  {
+    id:2,
+    title: "section3",
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex!lorem  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, totam quis deserunt, velit nesciunt doloribus adipisci deleniti architecto sapiente porro nostrum voluptatum voluptates iste maiores iure voluptate. Recusandae quo iure, obcaecati omnis quaerat enim labore, cumque rerum commodi doloremque ad ipsum iusto possimus nam mollitia perferendis maiores error saepe debitis eaque repellat. Officia cum totam minus aliquam eos illum sapiente voluptatem vel inventore ad exercitationem distinctio ipsum rerum incidunt, cupiditate laudantium, tempore quaerat? Ducimus, error at, exercitationem, incidunt nihil suscipit maxime vel voluptas aperiam voluptatem sit iure esse voluptatum. Ipsa dolor nihil corporis sed nemo beatae, necessitatibus delectus asperiores molestias recusandae ea modi ad pariatur fugit eveniet quisquam consectetur vel harum laudantium itaque qui dolores consequatur magnam alias. Recusandae debitis rerum unde autem eaque assumenda vitae nostrum quia beatae ex repellat natus, porro voluptates ipsam facilis. Reprehenderit magnam explicabo voluptatum rem itaque totam hic. Ab, optio molestiae reprehenderit distinctio ipsam repudiandae suscipit accusantium incidunt consectetur nostrum sit atque nisi voluptatum, corporis dolorem ea consequuntur aspernatur nulla facilis. Nostrum nihil fugit ipsa doloremque labore tenetur nemo consequuntur blanditiis ex odio, laborum ducimus distinctio voluptatem placeat perferendis! Aliquid nesciunt placeat eum nulla beatae distinctio cupiditate fugit blanditiis? Fugiat, asperiores saepe. Amet, aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex",
+  },
+  {
+    id:3,
+    title: "section4",
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sLorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laudantium, sed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, totam quis deserunt, velit nesciunt doloribus adipisci deleniti architecto sapiente porro nostrum voluptatum voluptates iste maiores iure voluptate. Recusandae quo iure, obcaecati omnis quaerat enim labore, cumque rerum commodi doloremque ad ipsum iusto possimus nam mollitia perferendis maiores error saepe debitis eaque repellat. Officia cum totam minus aliquam eos illum sapiente voluptatem vel inventore ad exercitationem distinctio ipsum rerum incidunt, cupiditate laudantium, tempore quaerat? Ducimus, error at, exercitationem, incidunt nihil suscipit maxime vel voluptas aperiam voluptatem sit iure esse voluptatum. Ipsa dolor nihil corporis sed nemo beatae, necessitatibus delectus asperiores molestias recusandae ea modi ad pariatur fugit eveniet quisquam consectetur vel harum laudantium itaque qui dolores consequatur magnam alias. Recusandae debitis rerum unde autem eaque assumenda vitae nostrum quia beatae ex repellat natus, porro voluptates ipsam facilis. Reprehenderit magnam explicabo voluptatum rem itaque totam hic. Ab, optio molestiae reprehenderit distinctio ipsam repudiandae suscipit accusantium incidunt consectetur nostrum sit atque nisi voluptatum, corporis dolorem ea consequuntur aspernatur nulla facilis. Nostrum nihil fugit ipsa doloremque labore tenetur nemo consequuntur blanditiis ex odio, laborum ducimus distinctio voluptatem placeat perferendis! Aliquid nesciunt placeat eum nulla beatae distinctio cupiditate fugit blanditiis? Fugiat, asperiores saepe. Amet, aut.ed aperiam temporibus saepe amet veritatis asperiores est fugit expedita numquam beatae, blanditiis quae incidunt doloribus ipsam esse et ex",
+  },
+  
+];
+
+
+
+
+
+// loop on nav to displat links in nav
+let nav=document.getElementById("nav")
+
+for(section in data){
+  nav.innerHTML+=`
+    <li><a href="#section${section}">${data[section].title}</a></li>
+  `
+}
+
+
+//hover nav at mobile responsive
+  let lis =document.querySelectorAll("li");
+
+lis.forEach(e => {
+  e.onmouseover=()=>{
+    e.classList.add("active-hover");
+  }
+  e.onmouseleave = () => {
+    e.classList.remove("active-hover");
+  };
+});
+
+
+//loop on data and display it
+
+let sectionsCon=document.getElementById("sec-con");
+for(section in data){
+  sectionsCon.innerHTML += `
+  <div id="section${section}" class="section">
+    <h1 class="title" >${data[section].title}</h1>
+    <span class="line"></span>
+    <p class="desvription">${data[section].description}</p>
+  </div>
+  `;
+}
+
+// active selction
+lis.forEach((e)=>{
+  e.onclick=()=>{
+    lis.forEach((e)=>{
+      e.classList.remove("avtive-link");
+    })
+    e.classList.add("avtive-link");
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+## this app is developed by youssef askar##
+
+<!-- ============| my data |============-->
+# i make my data in  array of obj 
+
+<!-- ============| nav bar |============-->
+# my nav bar created to contain any sections that can added in the future by the back-end
+
+
+<!-- ============| main |============-->
+# i make for  loop to display all the data in my array which will be good with dealing with apis
+
+<!-- ============| finally |============-->
+# iadded some css styles like hover 
+# make some styles with js 
+
+
+
